d422e1a4a5165dc80e08f3f1aae7fad0b78b4cc0
Amit yadav <114279131+Amityadavvns@users.noreply.github.com>
2022-12-27 00:00:24 +0530

    firstupload

---
+++ first.js
@@ -0,0 +1,34 @@
+/*console.log("hello worhld");
+let a = 5;
+console.log(a);
+let b = "amityadav";
+console.log(a);
+{
+  let a = "rohit";
+  console.log(a);
+}
+console.log(a);
+nn ss bb u primitive data type in js */
+let a = null;
+let b = 235;
+let c = true;
+let d = BigInt("165") + BigInt("6");
+let e = "amit";
+let f = Symbol("i am amit");
+let g;
+console.table([a, b, c, d, e, f, g]);
+console.log(typeof c);
+// non primitive data type is a Object in JS
+const item = {
+  amit: 65,
+  rohit: 47,
+  akhil: undefined,
+  harsh: false,
+};
+console.log(item["harsh"]);
+//hitesh choudhry
+const acountId = 12422;
+let acountEmail = "mjamit1947@gmail.com";
+let acountPassword = "154255";
+acountCity = "rewa";
+console.table([acountId, acountEmail, acountPassword, acountCity]);
